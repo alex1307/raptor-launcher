94dc88dcc150109aba91f709fc3d7322b0d3ed0a
alex1307 <alexander.todorov@ayagasha.com>
2025-10-25 01:35:17 +0300

    Run on ubuntu

--- start_app.sh
+++ start_app.sh
@@ -2,4 +2,4 @@ set -a; \
 source devops/env/raptor.systemd.env; \
 source devops/env/.systemd.env; \
 set +a; \
-./bin/raptor_launcher foreground
+/home/matkat/launcher-app/bin/raptor_launcher foreground
