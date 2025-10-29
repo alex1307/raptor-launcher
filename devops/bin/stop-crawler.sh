f3f361decd38183aaf51e540cf1f7c84b41d113b
alex1307 <alexander.todorov@ayagasha.com>
2025-10-29 18:14:42 +0200

    Fix stop crawler.sh

--- devops/bin/stop-crawler.sh
+++ devops/bin/stop-crawler.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 set -e
+
+OS_TYPE="$(uname)"
+if [[ "$OS_TYPE" == "Darwin" ]]; then
+  CRAWLER_HOME="${MAC_CRAWLER_HOME:-/Users/matkat/Software/docker-env/crawler}"
+elif [[ "$OS_TYPE" == "Linux" ]]; then
+  CRAWLER_HOME="${UBUNTU_CRAWLER_HOME:-/home/matkat/crawler-app}"
+else
+  echo "Unsupported OS: $OS_TYPE"
+  exit 1
+fi
+
 exec >> "$CRAWLER_HOME/_logs/crawler.log" 2>&1
 
 echo "[$(date '+%Y-%m-%d %H:%M:%S')] Stopping crawler..."
